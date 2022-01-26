3854de23ef8596df7aaa8860503ea5cd35b31c89
JZ1229 <jz2420@ic.ac.uk>
2022-01-26 18:42:56 +0000

    Addition and equality works

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -1,3 +1,6 @@
+from numbers import Number
+
+
 class Polynomial :
     
     def __init__(self , coefs):
@@ -21,4 +24,28 @@ class Polynomial :
 
         terms += [f"{'' if c==1 else c}x^{d}" for d,c in enumerate(coefs[2:], start=2) if c]
 
-        return '+'.join(reversed(terms)) or '0'
+        return '+'.join(reversed(terms)) or '0' 
+
+    def __eq__(self, other):
+
+        return self.coefficients == other.coefficients  
+
+
+    def __add__(self, other):
+        
+        if isinstance(other, Polynomial):
+            common = min(other.degree(), self.degree()) + 1
+            coefs = tuple(a + b for a ,b in zip(self.coefficients[:common], other.coefficients[:common]))
+            coefs += other.coefficients[common:] + self.coefficients[common:]
+            return Polynomial(coefs)
+
+        elif isinstance(other, Number):
+            return Polynomial((self.coefficients[0] + other,) + self.coefficients[1:])
+
+        else:
+            return NotImplemented
+
+    def __radd__(self,other):
+        return self + other 
+
+
